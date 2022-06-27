2143a8fd27c09a2d1843a691146e42b2d67371ec
YORKSHIRE-TEA-G\domin <blust.nick@gmail.com>
2022-06-27 23:43:08 +0200

    Added images for the tiles (64x64 pixels, because 32x32 just is too small).

--- src/LogicBoard/BoardGUI.java
+++ src/LogicBoard/BoardGUI.java
@@ -24,8 +24,8 @@ public class BoardGUI extends JFrame
      * specifically the display height and width of tiles on the board. Tile sizes 
      * should match the size of the image files used.
      */
-    public static final int TILE_WIDTH = 32;
-    public static final int TILE_HEIGHT = 32;
+    public static final int TILE_WIDTH = 64;
+    public static final int TILE_HEIGHT = 64;
 
     /**
      * The canvas is the area that graphics are drawn to. It is an internal class
@@ -66,14 +66,38 @@ public class BoardGUI extends JFrame
 /**
  * Internal class used to draw elements within a JPanel. The Canvas class loads
  * images from an asset folder inside the main project folder.
- * @author cmcgrego
+ * @author cmcgrego, Dominik Baumann
  */
 class Canvas extends JPanel {
 
     private BufferedImage tileEmpty;
-    private BufferedImage tileOR; //TO BE ADDED
-    private BufferedImage tileAND; //TO BE ADDED
-
+    private BufferedImage tileFALSE;
+    private BufferedImage tileTRUE;
+    
+    private BufferedImage tileAND_White;
+    private BufferedImage tileAND_TRUE;
+    private BufferedImage tileAND_FALSE;
+    
+    private BufferedImage tileNAND_White;
+    private BufferedImage tileNAND_TRUE;
+    private BufferedImage tileNAND_FALSE;
+    
+    private BufferedImage tileNOR_White;
+    private BufferedImage tileNOR_TRUE;
+    private BufferedImage tileNOR_FALSE;
+    
+    private BufferedImage tileNOT_White;
+    private BufferedImage tileNOT_TRUE;
+    private BufferedImage tileNOT_FALSE;
+    
+    private BufferedImage tileOR_White;
+    private BufferedImage tileOR_TRUE;
+    private BufferedImage tileOR_FALSE;
+    
+    private BufferedImage tileXOR_White;
+    private BufferedImage tileXOR_TRUE;
+    private BufferedImage tileXOR_FALSE;
+    
     TileType[][] currentTiles;  //the current 2D array of tiles to display
 
     
@@ -90,21 +114,56 @@ class Canvas extends JPanel {
      */
     private void loadTileImages() {
         try { // Took code for generating the filePath from here: https://stackoverflow.com/a/15804263
-        	String absolutePath = new File(".").getAbsolutePath();
-//        	System.out.println(absolutePath);// Shows you the path of your Project Folder
-        	int last = absolutePath.length()-1;
-        	absolutePath = absolutePath.substring(0, last);//Remove the dot at the end of path
-//        	System.out.println(absolutePath);
-        	String filePath =  "bin\\Assets\\tileEmpty.png";//You know this
-//        	System.out.println(absolutePath + filePath);//Get the full path.
-//            tileEmpty = ImageIO.read(new File("assets/tileEmpty.png"));
-        	tileEmpty = ImageIO.read(new File(absolutePath + filePath));
+        	
+        	// path of project Folder
+        	String absolutePath = new File(".").getAbsolutePath(); 
+        	
+        	// remove the dot at the end of path
+        	absolutePath = absolutePath.substring(0, absolutePath.length() - 1);
+        	tileEmpty = ImageIO.read(new File(absolutePath + "bin\\Assets\\tileEmpty64.png"));
+            
+        	tileTRUE = ImageIO.read(new File(absolutePath + "bin\\Assets\\TRUE.png"));
+            tileFALSE = ImageIO.read(new File(absolutePath + "bin\\Assets\\FALSE.png"));
+        	
+            tileAND_White = ImageIO.read(new File(absolutePath + "bin\\Assets\\AND_White.png"));
+            tileAND_TRUE = ImageIO.read(new File(absolutePath + "bin\\Assets\\AND_TRUE.png"));
+            tileAND_FALSE = ImageIO.read(new File(absolutePath + "bin\\Assets\\AND_FALSE.png"));
+            
+            tileNAND_White = ImageIO.read(new File(absolutePath + "bin\\Assets\\NAND_White.png"));
+            tileNAND_TRUE = ImageIO.read(new File(absolutePath + "bin\\Assets\\NAND_TRUE.png"));
+            tileNAND_FALSE = ImageIO.read(new File(absolutePath + "bin\\Assets\\NAND_FALSE.png"));
+            
+            tileNOR_White = ImageIO.read(new File(absolutePath + "bin\\Assets\\NOR_White.png"));
+            tileNOR_TRUE = ImageIO.read(new File(absolutePath + "bin\\Assets\\NOR_TRUE.png"));
+            tileNOR_FALSE = ImageIO.read(new File(absolutePath + "bin\\Assets\\NOR_FALSE.png"));
+            
+            tileNOT_White = ImageIO.read(new File(absolutePath + "bin\\Assets\\NOT_White.png"));
+            tileNOT_TRUE = ImageIO.read(new File(absolutePath + "bin\\Assets\\NOT_TRUE.png"));
+            tileNOT_FALSE = ImageIO.read(new File(absolutePath + "bin\\Assets\\NOT_FALSE.png"));
+            
+            tileOR_White = ImageIO.read(new File(absolutePath + "bin\\Assets\\OR_White.png"));
+            tileOR_TRUE = ImageIO.read(new File(absolutePath + "bin\\Assets\\OR_TRUE.png"));
+            tileOR_FALSE = ImageIO.read(new File(absolutePath + "bin\\Assets\\OR_FALSE.png"));
+            
+            tileXOR_White = ImageIO.read(new File(absolutePath + "bin\\Assets\\XOR_White.png"));
+            tileXOR_TRUE = ImageIO.read(new File(absolutePath + "bin\\Assets\\XOR_TRUE.png"));
+            tileXOR_FALSE = ImageIO.read(new File(absolutePath + "bin\\Assets\\XOR_FALSE.png"));
+            
+            // only need temporarily for size verification of the images
+            BufferedImage[] temp = { tileEmpty, tileFALSE, tileTRUE,
+            		    tileAND_White, tileAND_TRUE, tileAND_FALSE,
+            		    tileNAND_White, tileNAND_TRUE, tileNAND_FALSE,
+            		    tileNOR_White, tileNOR_TRUE, tileNOR_FALSE,
+            		    tileNOT_White, tileNOT_TRUE, tileNOT_FALSE,
+            		    tileOR_White, tileOR_TRUE, tileOR_FALSE,
+            		    tileXOR_White, tileXOR_TRUE, tileXOR_FALSE };
+            
+            for(BufferedImage b : temp) {
+              assert b.getHeight() == BoardGUI.TILE_HEIGHT &&
+              b.getWidth() == BoardGUI.TILE_WIDTH;
+            }
 
-            assert tileEmpty.getHeight() == BoardGUI.TILE_HEIGHT &&
-                    tileEmpty.getWidth() == BoardGUI.TILE_WIDTH;
-
-        } catch (IOException e) 
-        {
+        } catch (IOException e) {
             System.out.println("Exception loading images: " + e.getMessage());
             e.printStackTrace(System.out);
         }
