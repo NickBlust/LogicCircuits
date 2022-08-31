433925878cdd7dae4e893d64923da8e573dc793b
philipp-a-grz <106536095+philipp-a-grz@users.noreply.github.com>
2022-08-31 18:45:18 +0200

    Create NORgateTest.java

---
+++ src/gates/NORgateTest.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package gates;
+
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author philipp
+ * This tests the methods which are coded within the class NORgate for their "isolated"
+ * functionality; while general gate methods are tested in {@link gates.JUnit_Test_Gates.java JUnit_Test_Gates}
+ *
+ */
+public class NORgateTest extends TestCase {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	/**
+	 * Test method for {@link gates.NORgate#name()}.
+	 */
+	@Test
+	public void testName() {
+		NORgate nor1 = new NORgate();
+		assertEquals("NOR", nor1.name()); 
+	}
+
+	/**
+	 * Test method for {@link gates.NORgate#computeOutput(boolean, boolean)}.
+	 */
+	@Test
+	public void testComputeOutput() {
+		NORgate nor1 = new NORgate();
+		assertEquals(false, nor1.computeOutput(true, true));
+		assertEquals(false, nor1.computeOutput(false, true));
+		assertEquals(false, nor1.computeOutput(true, false));
+		assertEquals(true, nor1.computeOutput(false, false));
+	}
+
+}
