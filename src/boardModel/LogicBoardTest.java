28edd150c3b1c372b984965cbeba15ef05b3b953
philipp-a-grz <106536095+philipp-a-grz@users.noreply.github.com>
2022-09-07 17:07:32 +0200

    Create LogicBoardTest.java

---
+++ src/boardModel/LogicBoardTest.java
@@ -0,0 +1,179 @@
+/**
+ * 
+ */
+package boardModel;
+
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author philb
+ *
+ */
+public class LogicBoardTest extends TestCase {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	/**
+	 * Test method for {@link boardModel.LogicBoard#addGate(gates.Gate, utility.Vector2Int)}.
+	 */
+	@Test
+	public void testAddGate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link boardModel.LogicBoard#removeGate(utility.Vector2Int)}.
+	 */
+	@Test
+	public void testRemoveGate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link boardModel.LogicBoard#addConnection(utility.Vector2Int, utility.Vector2Int, gates.GateIndex)}.
+	 */
+	@Test
+	public void testAddConnection() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link boardModel.LogicBoard#removeConnection(utility.Vector2Int, gates.GateIndex)}.
+	 */
+	@Test
+	public void testRemoveConnection() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link boardModel.LogicBoard#attemptConnectionRemoval(utility.Vector3Int, command.TryToRemoveConnection)}.
+	 */
+	@Test
+	public void testAttemptConnectionRemoval() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link boardModel.LogicBoard#evaluate()}.
+	 */
+	@Test
+	public void testEvaluate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link boardModel.LogicBoard#formsCycle(utility.Vector2Int, utility.Vector2Int, gates.GateIndex, gates.GateIndex)}.
+	 */
+	@Test
+	public void testFormsCycle() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link boardModel.LogicBoard#reset()}.
+	 */
+	@Test
+	public void testReset() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link boardModel.LogicBoard#numberOfGates()}.
+	 */
+	@Test
+	public void testNumberOfGates() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link boardModel.LogicBoard#setGates(java.util.TreeMap, java.util.HashMap)}.
+	 */
+	@Test
+	public void testSetGates() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link boardModel.LogicBoard#getGates()}.
+	 */
+	@Test
+	public void testGetGates() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link boardModel.LogicBoard#getOutputGates()}.
+	 */
+	@Test
+	public void testGetOutputGates() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link boardModel.LogicBoard#hasGate(utility.Vector2Int)}.
+	 */
+	@Test
+	public void testHasGate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link boardModel.LogicBoard#getGate(utility.Vector2Int)}.
+	 */
+	@Test
+	public void testGetGate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link boardModel.LogicBoard#getGateType(utility.Vector2Int)}.
+	 */
+	@Test
+	public void testGetGateType() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link boardModel.LogicBoard#getPositionOfGate(gates.Gate)}.
+	 */
+	@Test
+	public void testGetPositionOfGate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link boardModel.LogicBoard#toStorageString()}.
+	 */
+	@Test
+	public void testToStorageString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link boardModel.LogicBoard#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
